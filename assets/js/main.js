6e02a82b89235130b2644416610f599351c485bd
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-13 17:57:52 +0100

    fix for

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,60 +70,66 @@ const mailAccess = [
     'pluto@gmail.com',
     'paperino@gmail.com'
 ];
-console.log(mailAccess);
+
 
 
 const buttonEl = document.querySelector('button');
 const h1Element = document.querySelector('h1');
 
 //creare un addEventListener per inviare la mail
-buttonEl.addEventListener ('click', function(){
+buttonEl.addEventListener('click', function () {
     const mailUser = document.querySelector('input').value
     console.log(mailUser);
 
+    let hasAcces = false;
     //creare un ciclo per il controllo della mai
     for (let i = 0; i < mailAccess.length; i++) {
-
-        //controllare se la mail sia nella lista di chi può accedere o meno
-        if (mailUser == mailAccess[0],[1],[2]) {     
-    
-            //stampare un messaggio che indichi che si può accedere
-            h1Element.innerHTML = 'complimenti sei in lista';
-    
-        } else {
-    
-            //stampare un messaggio che indichi che non si può accedere
-            h1Element.innerHTML = 'mi dispiace non sei in lista';
+        console.log(mailAccess[i]);
+        if (mailUser == mailAccess[i]) {
+           hasAcces = true;
         }
+        
+    }
+
+    //controllare se la mail sia nella lista di chi può accedere o meno
+    if (hasAcces) {
+
+        //stampare un messaggio che indichi che si può accedere
+        h1Element.innerHTML = 'complimenti sei in lista';
+
+    } else {
+
+        //stampare un messaggio che indichi che non si può accedere
+        h1Element.innerHTML = 'mi dispiace non sei in lista';
     }
 
 
-//DADI
+    //DADI
 
-//selezionare l'elemento in dom nel quale stampare il risultato della sfida
-const h2Element = document.querySelector('h2')
+    //selezionare l'elemento in dom nel quale stampare il risultato della sfida
+    const h2Element = document.querySelector('h2')
 
-//generare con math.random un numero che va da 1 a 6 per l'utente e loggarlo
-const userDice = Math.floor(Math.random() * 6) + 1;
-console.log(userDice);
+    //generare con math.random un numero che va da 1 a 6 per l'utente e loggarlo
+    const userDice = Math.floor(Math.random() * 6) + 1;
+    console.log(userDice);
 
-//generare con math.random un numero che va da 1 a 6 per il computer e loggarlo
-const computerDice = Math.floor(Math.random() * 6) + 1;
-console.log(computerDice);
+    //generare con math.random un numero che va da 1 a 6 per il computer e loggarlo
+    const computerDice = Math.floor(Math.random() * 6) + 1;
+    console.log(computerDice);
 
-//verificare quale dei due numeri è maggiore
-if (userDice > computerDice) {
-    console.log('HAI VINTO');
-    h2Element.innerHTML = 'HAI VINTO';
-    
-} else if (computerDice > userDice) {
-    console.log('HAI PERSO');
-    h2Element.innerHTML = 'HAI PERSO';
+    //verificare quale dei due numeri è maggiore
+    if (userDice > computerDice) {
+        console.log('HAI VINTO');
+        h2Element.innerHTML = 'HAI VINTO';
 
-} else {
-    console.log('PAREGGIO');
-    h2Element.innerHTML = 'PAREGGIO';
-}
+    } else if (computerDice > userDice) {
+        console.log('HAI PERSO');
+        h2Element.innerHTML = 'HAI PERSO';
+
+    } else {
+        console.log('PAREGGIO');
+        h2Element.innerHTML = 'PAREGGIO';
+    }
 
 })
 
